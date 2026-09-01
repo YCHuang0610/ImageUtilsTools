1f29b61f7bce942be835c4109d8f0dbb8437c583
Yichun Huang <ychunhuang@foxmail.com>
2026-09-01 10:40:59 +0800

    Version to 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ImageUtilsTools",
-    version="0.2.0",
+    version="0.2.1",
     description="...",
     author="Yichun Huang",
     author_email="ychunhuang@foxmail.com",
